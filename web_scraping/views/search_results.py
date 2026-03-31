2ef3fc3606124f40092e1b55a164102e099cc23c
Sergey' home <ssaltykov8@mail.ru>
2026-03-31 21:23:18 +0300

    v4

--- web_scraping/views/search_results.py
+++ web_scraping/views/search_results.py
@@ -1,22 +1,40 @@
 from django.shortcuts import render
-from web_scraping.models import Product
+from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ObjectDoesNotExist
+from web_scraping.models import Product, Marketplace
 
 def search_results(request):
+    query = request.GET.get('query', '').strip()
+    marketplaces = request.GET.getlist('marketplaces[]')
 
-    query = request.GET.get('query','')
-    platforms = request.GET.getlist('platforms')
-    print("Query:", request.GET.get('query'))
-    print("Platforms:", request.GET.getlist('platforms'))
+    # Получаем все площадки для отображения в фильтре
+    all_marketplaces = Marketplace.objects.all()
 
-    products = Product.objects.filter(
-        name__icontains=query
-    )
-    if platforms:
-        products = products.filter(platform__in=platforms)
+    marketplace_ids = []
+    for marketplace_name in marketplaces:
+        try:
+            marketplace = Marketplace.objects.get(name=marketplace_name)
+            marketplace_ids.append(marketplace.id)
+        except ObjectDoesNotExist:
+            pass
+
+    print("Request method:", request.method)
+    print("Full GET data:", request.GET)
+    print("Query:", query)
+    print("Marketplaces_ids:", marketplaces)
+
+    products = Product.objects.filter(name__icontains=query)
+
+    if marketplace_ids:
+        products = products.filter(marketplace__in=marketplace_ids)
+
+    print("Products:", products.count())
 
     context = {
         'products': products,
         'query': query,
+        'all_marketplaces': all_marketplaces,
+        'selected_marketplaces': marketplaces,  # Для отметки выбранных площадок
     }
 
     return render(request, 'web_scarping/search_results.html', context=context)
