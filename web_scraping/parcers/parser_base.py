517180d8cc6bb3b45de41d2369300b97f56dcd66
Greencapral <ssaltykov8@mail.ru>
2026-04-22 10:40:47 +0300

    everything working v44

--- web_scraping/parcers/parser_base.py
+++ web_scraping/parcers/parser_base.py
@@ -109,7 +109,7 @@ class Parser:
         USER_AGENTS = [
             # Desktop
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6099.224 Safari/537.36",
-            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15",
+            # "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Safari/605.1.15",
             # "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6099.224 Safari/537.36",
 
             # New ones
@@ -267,7 +267,7 @@ class Parser:
             except Exception:  # Перехватываем любые исключения при закрытии драйвера
                 pass  # Игнорируем ошибки — главное, чтобы не прерывать выполнение программы
 
-    def human_like_actions(self, pages_to_scroll=3):
+    def human_like_actions(self, pages_to_scroll=2):
         """
         Имитирует человеческие действия в браузере: движения курсора и скроллинг страницы.
         Args:
